757cbd7cc8467cbc7b093ac30fca27ecc3c4ffe3
Altaha <altahabinsyafriadi@gmail.com>
2021-11-21 00:02:35 +0700

    fixing bugs at post section

--- factory/factory.go
+++ factory/factory.go
@@ -1,24 +1,24 @@
 package factory
 
 import (
-	_post_business "mbpe/features/articles/bussiness"
-	_post_data "mpbe/features/articles/data"
+	_post_business "mbpe/features/posts/bussiness"
+	_post_data "mpbe/features/posts/data"
 
-	_post_presentation "mbpe/features/articles/presentation"
+	_post_presentation "mbpe/features/posts/presentation"
 	"mpbe/config"
 )
 
 type Presenter struct {
-	ArticlePresentation *_article_presentation.ArticlesHandler
+	PostPresentation *_post_presentation.PostsHandler
 }
 
 func Init() Presenter {
 
-	postData := _post_data.NewArticleRepository(config.DB)
+	postData := _post_data.NewPostRepository(config.DB)
 	postBussiness := _post_business.NewAricleBussiness(postData)
-	postPresentation := _post_presentation.NewArticleHandler(postBussiness)
+	postPresentation := _post_presentation.NewPostHandler(postBussiness)
 
 	return Presenter{
-		ArticlePresentation: postPresentation,
+		PostPresentation: postPresentation,
 	}
 }
