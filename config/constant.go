ef09185bb7f673740d6c8226a42cbfe6c28a745b
Altaha <altahabinsyafriadi@gmail.com>
2021-11-29 18:54:21 +0700

    rewrite codes

--- config/constant.go
+++
@@ -1,2 +0,0 @@
-package config
-const JWT_SECRET = "p1a3d5a7s9u8a6t4u2h0a3r4i5"
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"antonio/driver"
-	"antonio/routes"
 )
 
 func main() {
-	driver.InitDB()
-	e:=routes.NewRoutes()
-	e.Start("localhost:8000")
+
 }
