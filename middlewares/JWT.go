1719b22eafc2bccea2609e61c93ba4c54a523f38
Altaha <altahabinsyafriadi@gmail.com>
2021-12-01 17:43:10 +0700

    fixing bugs again

--- middlewares/JWT.go
+++ middlewares/JWT.go
@@ -26,33 +26,6 @@ func CreateToken(userId int) (string, error) {
 	return token.SignedString([]byte(constants.ACCESS_TOKEN_KEY))
 }
 
-// func CreateRefreshToken(userId int) (string, error) {
-// 	claims := &JwtCustomClaims{
-// 		userId,
-// 		jwt.StandardClaims{
-// 			ExpiresAt: time.Now().Add(time.Hour).Unix(),
-// 		},
-// 	}
-
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-// 	return token.SignedString([]byte(constants.REFRESH_TOKEN_KEY))
-// }
-
-// func VerifyRefreshToken(refreshToken string) (userId int, err error) {
-// 	keyFunc := func(t *jwt.Token) (interface{}, error) {
-// 		return []byte(constants.REFRESH_TOKEN_KEY), nil
-// 	}
-// 	jwtToken, err := jwt.ParseWithClaims(refreshToken, &JwtCustomClaims{}, keyFunc)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-
-// 	claims := jwtToken.Claims.(*JwtCustomClaims)
-// 	userId = claims.UserId
-
-// 	return userId, nil
-
-// }
 
 func VerifyAccessToken(c echo.Context) (userId int, err error) {
 	keyFunc := func(t *jwt.Token) (interface{}, error) {
