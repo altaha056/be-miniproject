ba53be2e4c53162052a7eae99d77d57ce454559d
Altaha <altahabinsyafriadi@gmail.com>
2021-12-04 15:24:19 +0700

    Update usecase_test.go

--- features/comments/business/usecase_test.go
+++ features/comments/business/usecase_test.go
@@ -2,17 +2,157 @@ package business
 
 import (
 	"antonio/features/comments"
-	"antonio/features/contents/mocks"
+	"antonio/features/comments/mocks"
+	"errors"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 var (
-	commentService 		comments.Business
-	commentRepository	mocks.Data
+	commentsRepo		mocks.Data
+	commentsBusiness	comments.Business
 	commentData			comments.Core
-	commentsData		comments.Core
+	commentsData		[]comments.Core
 )
 
 func TestMain(m *testing.M){
+	commentsBusiness=NewCommentsBusiness(&commentsRepo)
+	commentData = comments.Core{
+		ID: 1,
+		Content: "tes komentar 1",
+		UserID: 1,
+		User: comments.UserCore{
+			ID: 1,
+			Email: "altaha@alta.com",
+			Username: "altaha",
+		},
+		ArticleID: 1,
+		Article: comments.ArticleCore{
+			ID: 1,
+			Title: "judul konten 1",
+			Image: "gambar konten 1",
+			Content: "isi deskripsi konten 1",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			User: comments.UserCore{
+				ID: 1,
+				Email: "alta2@alta.com",
+				Username: "tes",
+			},
+			Tags: []comments.TagCore{
+				{
+					ID: 1,
+					Title: "photoshop",
+				},
+				{
+					ID: 2,
+					Title: "illustrator",
+				},
+			},
+		},
+	}
 	
+	commentsData = []comments.Core{{ID: 1,
+		Content: "tes komentar 3",
+		UserID: 1,
+		User: comments.UserCore{
+			ID: 1,
+			Email: "altaha@alta.com",
+			Username: "altaha",
+		},
+		ArticleID: 1,
+		Article: comments.ArticleCore{
+			ID: 1,
+			Title: "judul konten 1",
+			Image: "gambar konten 1",
+			Content: "isi deskripsi konten 1",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			User: comments.UserCore{
+				ID: 1,
+				Email: "alta2@alta.com",
+				Username: "tes",
+			},
+			Tags: []comments.TagCore{
+				{
+					ID: 1,
+					Title: "photoshop",
+				},
+				{
+					ID: 2,
+					Title: "illustrator",
+				},
+			},
+		},},{ID: 1,
+		Content: "tes komentar 4",
+		UserID: 1,
+		User: comments.UserCore{
+			ID: 1,
+			Email: "altaha@alta.com",
+			Username: "altaha",
+		},
+		ArticleID: 1,
+		Article: comments.ArticleCore{
+			ID: 1,
+			Title: "judul konten 1",
+			Image: "gambar konten 1",
+			Content: "isi deskripsi konten 1",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			User: comments.UserCore{
+				ID: 1,
+				Email: "alta2@alta.com",
+				Username: "tes",
+			},
+			Tags: []comments.TagCore{
+				{
+					ID: 1,
+					Title: "photoshop",
+				},
+				{
+					ID: 2,
+					Title: "illustrator",
+				},
+			},
+		},}}
+
+	os.Exit(m.Run())
 }
+
+func TestAddComment(t *testing.T){
+	t.Run("comment article success", func(t *testing.T) {
+		commentsRepo.On("AddComment", mock.AnythingOfType("content string, articleId int, userId int")).Return(nil).Once()
+		err := commentsBusiness.AddComment(commentData.Content, commentData.ArticleID, commentData.UserID)
+		assert.Nil(t, err)
+	})
+
+	t.Run("comment article fail", func(t *testing.T) {
+		commentsRepo.On("AddComment", mock.AnythingOfType("content string, articleId int, userId int")).Return(errors.New("comment article fail")).Once()
+		err := commentsBusiness.AddComment(commentData.Content, commentData.ArticleID, commentData.UserID)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "comment article fail")
+	})
+
+}
+
+func TestDeleteComment(t *testing.T) {
+
+	t.Run("delete comment success", func(t *testing.T) {
+		commentsRepo.On("DeleteComment", mock.AnythingOfType("int")).Return(nil).Once()
+		err := commentsBusiness.DeleteComment(1,1)
+		assert.Nil(t, err)
+	})
+
+	t.Run("delete comment fail", func(t *testing.T) {
+		commentsRepo.On("DeleteComment", mock.AnythingOfType("int")).Return(errors.New("delete comment fail")).Once()
+		err := commentsBusiness.DeleteComment(1,1)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "delete comment fail")
+	})
+
+}
+
